0b765e652b29c356d8e087fc1aafc9eac2ae068b
Assis Ferreira <assisrunny@hotmail.com>
2020-09-10 13:14:34 -0100

    feat: Is Url Function

--- myjs.js
+++ myjs.js
@@ -71,7 +71,14 @@ myjs.isEmail = function(str)
 
 myjs.isUrl = function(str)
 {
-	
+	var pattern = new RegExp('^(https?:\\/\\/)?'+
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+
+    '(\\#[-a-z\\d_]*)?$','i');
+
+	return !!pattern.test(str);
 }
 
 myjs.isNumber = function(num)
