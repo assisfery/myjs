b453ae41285414816012b975f1a0b1bc30c2a95b
Assis Ferreira <assisrunny@hotmail.com>
2020-09-10 13:08:12 -0100

    feat: Is Email Function

--- myjs.js
+++ myjs.js
@@ -65,7 +65,8 @@ return decodeURIComponent(results[2].replace(/\+/g, ' '));
 // VALIDATIONS
 myjs.isEmail = function(str)
 {
-	
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(str).toLowerCase());
 }
 
 myjs.isUrl = function(str)
